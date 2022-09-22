d16f549545be830d5d47ee2bd34aa8fcdbbc40e2
Ashok <ashokmarmath8@gmail.com>
2022-09-22 08:38:45 +0530

    dfghjkjh

---
+++ src/lib/DialogModal.svelte
@@ -0,0 +1,43 @@
+/ 
+ <script  >
+ export let open = false;
+
+ import {
+    Dialog,
+    DialogOverlay,
+    PopoverOverlay,
+    TransitionChild,
+    TransitionRoot,
+  } from '@rgossiaux/svelte-headlessui';
+ </script>
+
+ <TransitionRoot as="div" show={open} >
+     <Dialog as="div" class="fixed inset-0 z-40" on:close={() => (open = false)} static>
+      <TransitionChild
+        as="div"
+        enter="transition-opacity ease-linear duration-200"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity ease-linear duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <DialogOverlay class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+      <TransitionChild
+        as="div"
+        enter="transition ease-linear duration-200 transform"
+        enterFrom=" -translate-x-full opacity-0 "
+        enterTo="opacity-100 translate-x-0"
+        leave=" transition transition-opacity ease-linear duration-200 transform"
+        leaveFrom="opacity-100 translate-x-0"
+        leaveTo="opacity-0 -translate-x-full"
+        class="w-full h-full"
+      >
+      <div class="w-full h-full bg-blue-500">
+      fgfasdfasdfasdf
+      </div>
+
+     </TransitionChild>
+    </Dialog>
+  </TransitionRoot>
---
+++ src/routes/index.svelte
@@ -0,0 +1,48 @@
+
+
+<script>
+	import DialogModal from '$lib/DialogModal.svelte'
+	let showModalA;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+
+<section>
+<DialogModal bind:open={showModalA}/>
+<button on:click={() => showModalA = true} class="bg-red-300">Open Modal</button>
+
+</section>
+<!-- 
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style> -->
